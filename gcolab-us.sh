a2c89c8430022d6b159a850094122d81b323d6e5
kouassi01n <102123184+kouassi01n@users.noreply.github.com>
2022-03-25 11:36:26 +0000

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -7,4 +7,4 @@ clear
 sudo apt install screen
 screen -R xmr
  
-!wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar -xf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BABYDOGE:0x8837dfc0bf0797e47dbd8a715c2d8f08e82e1ff1.Badotot
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar -xf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BABYDOGE:0x8837dfc0bf0797e47dbd8a715c2d8f08e82e1ff1.Badotot
